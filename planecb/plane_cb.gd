330db9c363fbccd18ea7e1a1e76a06f8c56fbb72
Thomas Gander - School <thomas.gander@mymhc.ca>
2023-10-23 14:56:13 -0600

    Got basic animations working.

--- planecb/plane_cb.gd
+++ planecb/plane_cb.gd
@@ -1,5 +1,10 @@
 extends CharacterBody2D
 
+@onready var animation_player = $AnimationPlayer
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+
+
 const GRAVITY: float = 1900.0
 const POWER: float = -400.0
 
@@ -12,8 +17,16 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	velocity.y += GRAVITY*delta
+	fly()
+	move_and_slide()
+	if is_on_floor() == true:
+		die()
 	
+func fly() -> void:
 	if Input.is_action_just_pressed("fly") == true:
 		velocity.y = POWER
-	
-	move_and_slide()
+		animation_player.play("fly");
+		
+func die() -> void:
+	animated_sprite_2d.stop()
+	set_physics_process(false)
